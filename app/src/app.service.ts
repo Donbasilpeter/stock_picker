89d763cafe07cb0ef5313be5e8c7a0a8653fcf90
DESKTOP-4SG5713\donba <92298873+donbasilpeter-aot@users.noreply.github.com>
2023-03-04 22:41:45 +0530

    inital http setup and lauch

--- app/src/app.service.ts
+++ app/src/app.service.ts
@@ -1,8 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { HttpService } from '@nestjs/axios';
+import { catchError, firstValueFrom } from 'rxjs';
+import  {AxiosError} from 'axios';
+
+import { GPU } from 'gpu.js';
+const gpu = new GPU();
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
-  }
+  constructor(private readonly httpService: HttpService) {}
+  async getHello(): Promise<any> {
+    const { data } = await firstValueFrom(
+      this.httpService.get('https://www.google.co.in/').pipe(
+        catchError((error: AxiosError) => {
+          console.log(error)
+          throw 'An error happened!';
+        }),
+      ),
+    );
+
+   return "nil"
+}
 }
