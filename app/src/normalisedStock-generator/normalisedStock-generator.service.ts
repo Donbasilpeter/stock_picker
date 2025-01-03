30d877a69bf767648bec24ad7d3de57f7913a5e0
Don Basil Peter <donbasilpeter@gmail.com>
2025-01-03 00:40:47 -0500

    Update normalisedStock-generator.service.ts

--- app/src/normalisedStock-generator/normalisedStock-generator.service.ts
+++ app/src/normalisedStock-generator/normalisedStock-generator.service.ts
@@ -106,67 +106,36 @@ searchStockList({ searchField, searchType, page }) {
   }
 }
 
-  createNormalisedStock() {
-    return this.NormalisedStockModel.collection
-      .drop()
-      .then(() => {
-        return this.PortfolioModel.collection.drop();
-      })
-      .then(() => {
-        return this.StockPriceModel.find({})
-          .lean()
-          .then((allData) => {
-            return allData.map((eachData) => {
-              return stockToNormalisedStock(eachData);
-            });
-          })
-          .then((normalisedStockOfOneStock) => {
-            let dailyMeanMin = getMin(normalisedStockOfOneStock, 'dailyMean');
-            let dailyMeanMax = getMax(normalisedStockOfOneStock, 'dailyMean');
-            let dailyStandardDeviationMin = getMin(
-              normalisedStockOfOneStock,
-              'dailyStandardDeviation',
-            );
-            let dailyStandardDeviationMax = getMax(
-              normalisedStockOfOneStock,
-              'dailyStandardDeviation',
-            );
-            return normalisedStockOfOneStock.map(
-              (eachNormalisedStock: NormalisedStockDto) => {
-                eachNormalisedStock.NormalisedDailyMean =
-                  (eachNormalisedStock.dailyMean - dailyMeanMin) /
-                    (dailyMeanMax - dailyMeanMin) +
-                  1;
-                eachNormalisedStock.NormalisedDailyStandardDeviation =
-                  (eachNormalisedStock.dailyStandardDeviation -
-                    dailyStandardDeviationMin) /
-                    (dailyStandardDeviationMax - dailyStandardDeviationMin) +
-                  1;
-                const createdstock = new this.NormalisedStockModel(
-                  eachNormalisedStock,
-                );
-                createdstock.save().catch((err) => {
-                  throw { status: err };
-                });
-                return eachNormalisedStock;
-              },
-            );
-          })
-          .then((normalisedStock) => {
-            return resultValidation(normalisedStock);
-          })
-          .catch((err) => {
-            console.log(err);
-            throw { status: err };
-          });
-      })
+async createNormalisedStock() {
+  try {
+    await this.NormalisedStockModel.collection.drop();
+    await this.PortfolioModel.collection.drop();
 
-      .catch((err) => {
-        console.log(err);
-        return err;
-      });
+    const allData = await this.StockPriceModel.find({}).lean();
+    const normalisedData = allData.map(stockToNormalisedStock);
+    const dailyMeanMin = getMin(normalisedData, 'dailyMean');
+    const dailyMeanMax = getMax(normalisedData, 'dailyMean');
+    const dailyStdDevMin = getMin(normalisedData, 'dailyStandardDeviation');
+    const dailyStdDevMax = getMax(normalisedData, 'dailyStandardDeviation');
+
+    const normalizedStocks = normalisedData.map((stock:NormalisedStockDto) => {
+      stock.NormalisedDailyMean =
+        (stock.dailyMean - dailyMeanMin) / (dailyMeanMax - dailyMeanMin) + 1;
+      stock.NormalisedDailyStandardDeviation =
+        (stock.dailyStandardDeviation - dailyStdDevMin) /
+          (dailyStdDevMax - dailyStdDevMin) +
+        1;
+      return stock;
+    });
+
+    await this.NormalisedStockModel.insertMany(normalizedStocks);
+
+    return resultValidation(normalizedStocks);
+  } catch (err) {
+    console.error('Error:', err);
+    throw err;
   }
-
+}
   analyseDonIntex({
     coefficent = 1,
     pfSize = 20,
