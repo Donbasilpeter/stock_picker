26ae0c83b66de6b673907d8dbde6207731358e68
Don Basil Peter <donbasilpeter@gmail.com>
2025-01-17 01:58:57 -0500

    remove wanrning

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -3,10 +3,7 @@ import mongoose from 'mongoose';
 // MongoDB connection initializer
 const initializeDatabase = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_DB_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.MONGO_DB_URL);
     console.log('Connected to MongoDB Atlas');
   } catch (err) {
     console.error('Error connecting to MongoDB:', err);
